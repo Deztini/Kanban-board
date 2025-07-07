ba9b983360bcc628365822e591d99233853b7b45
Deztini <agbatordestiny26@gmail.com>
2025-07-07 16:09:49 +0100

    added some fallback

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,7 @@
 import type { FC } from "react";
 import type { taskProps } from "../types/types";
+import { Ellipsis, Pencil, Trash } from "lucide-react";
+import { useState } from "react";
 
 // type TaskProps = {
 //   title: string;
@@ -8,12 +10,38 @@ import type { taskProps } from "../types/types";
 //   priority: string;
 // };
 
-const Task: FC<taskProps> = ({id, title, description, date, priority }) => {
+const Task: FC<taskProps> = ({ id, title, description, date, priority }) => {
+  const [isClicked, setIsClicked] = useState<boolean>();
+
+  function toggleOverlay() {
+    setIsClicked((prev) => !prev);
+  }
   return (
     <>
-      <div className="bg-black w-90 h-60 py-4 px-6 rounded-xl">
-        <h1 className="text-white text-2xl font-bold mb-2">{title}</h1>
-        <p className="text-[#ccc] font-bold mb-6">{description}</p>
+      <div className="bg-black w-90 h-60 py-4 px-6 rounded-xl relative">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-bold mb-2">{title}</h1>
+          <button className="text-white cursor-pointer" onClick={toggleOverlay}>
+            <Ellipsis />
+          </button>
+        </div>
+
+        <div className="flex justify-between">
+          <p className="text-[#ccc] font-bold mb-6">{description}</p>
+          {isClicked && (
+            <div className="bg-[#121212] h-28 w-35 flex flex-col gap-1 z-10 absolute top-13 right-5 rounded-xl px-3 py-3">
+              <button className="text-white cursor-pointer flex items-center gap-2 hover:bg-black py-2 px-2 rounded-xl">
+                {" "}
+                <Pencil style={{ color: "#3B82F6" }} /> Edit
+              </button>
+              <button className="text-white cursor-pointer flex items-center gap-2 hover:bg-black py-2 px-2 rounded-xl">
+                {" "}
+                <Trash style={{ color: "#EF4444" }} /> Delete
+              </button>
+            </div>
+          )}
+        </div>
+
         <div className="flex gap-4">
           <p className="rounded-4xl text-white bg-[#af74d7] py-2 px-2">
             {priority}
--- src/pages/Mainboard.tsx
+++ src/pages/Mainboard.tsx
@@ -5,117 +5,101 @@ import Modal from "../components/ui/Modal";
 import { useState } from "react";
 import ColorPicker from "../components/ColorPicker";
 import type { ProjectProps } from "../types/types";
+import boardImg from "../../public/assets/boardimage.jpg";
 
-const todoTasks = [
-  {
-    id: 1,
-    title: "Define Project Scope",
-    description:
-      "Outline the boundaries and objectives for the Q3 marketing campaign.",
-    date: "2025-06-23",
-    priority: "urgent",
-  },
-  {
-    id: 2,
-    title: "Research Competitors",
-    description: "Gather data on competitor strategies and market positioning.",
-    date: "2025-06-24",
-    priority: "Medium",
-  },
-  {
-    id: 3,
-    title: "Create Content Calendar",
-    description:
-      "Develop a detailed content schedule for social media and blog posts.",
-    date: "2025-06-25",
-    priority: "High",
-  },
-];
+// const todoTasks = [
+//   {
+//     id: 1,
+//     title: "Define Project Scope",
+//     description:
+//       "Outline the boundaries and objectives for the Q3 marketing campaign.",
+//     date: "2025-06-23",
+//     priority: "urgent",
+//   },
+//   {
+//     id: 2,
+//     title: "Research Competitors",
+//     description: "Gather data on competitor strategies and market positioning.",
+//     date: "2025-06-24",
+//     priority: "Medium",
+//   },
+//   {
+//     id: 3,
+//     title: "Create Content Calendar",
+//     description:
+//       "Develop a detailed content schedule for social media and blog posts.",
+//     date: "2025-06-25",
+//     priority: "High",
+//   },
+// ];
 
-const inprogressTasks = [
-  {
-    id: 4,
-    title: "Design UI Mockups",
-    description:
-      "Draft initial wireframes and high-fidelity mockups for the new feature.",
-    date: "2025-06-23",
-    priority: "Medium",
-  },
-  {
-    id: 5,
-    title: "Develop API Endpoints",
-    description: "Implement backend API for data retrieval and submission.",
-    date: "2025-06-24",
-    priority: "urgent",
-  },
-  {
-    id: 6,
-    title: "Write User Stories",
-    description:
-      "Document user journeys and requirements for upcoming sprints.",
-    date: "2025-06-24",
-    priority: "urgent",
-  },
-];
+// const inprogressTasks = [
+//   {
+//     id: 4,
+//     title: "Design UI Mockups",
+//     description:
+//       "Draft initial wireframes and high-fidelity mockups for the new feature.",
+//     date: "2025-06-23",
+//     priority: "Medium",
+//   },
+//   {
+//     id: 5,
+//     title: "Develop API Endpoints",
+//     description: "Implement backend API for data retrieval and submission.",
+//     date: "2025-06-24",
+//     priority: "urgent",
+//   },
+//   {
+//     id: 6,
+//     title: "Write User Stories",
+//     description:
+//       "Document user journeys and requirements for upcoming sprints.",
+//     date: "2025-06-24",
+//     priority: "urgent",
+//   },
+// ];
 
-const reviewTasks = [
-  {
-    id: 7,
-    title: "Peer Code Review",
-    description:
-      "Review new feature branch for quality and adherence to standards.",
-    date: "2025-06-20",
-    priority: "Low",
-  },
-  {
-    id: 8,
-    title: "Client Feedback Session",
-    description:
-      "Conduct a meeting to gather feedback on the latest prototype.",
-    date: "2025-06-20",
-    priority: "Low",
-  },
-];
+// const reviewTasks = [
+//   {
+//     id: 7,
+//     title: "Peer Code Review",
+//     description:
+//       "Review new feature branch for quality and adherence to standards.",
+//     date: "2025-06-20",
+//     priority: "Low",
+//   },
+//   {
+//     id: 8,
+//     title: "Client Feedback Session",
+//     description:
+//       "Conduct a meeting to gather feedback on the latest prototype.",
+//     date: "2025-06-20",
+//     priority: "Low",
+//   },
+// ];
 
-const completedTasks = [
-  {
-    id: 9,
-    title: "Project Kick-off Meeting",
-    description:
-      "Successfully initiated the new project with all stakeholders.",
-    date: "2025-06-29",
-    priority: "Low",
-  },
-  {
-    id: 10,
-    title: "Initial Market Research Report",
-    description: "Comprehensive report on initial market findings submitted.",
-    date: "2025-06-29",
-    priority: "Low",
-  },
-];
+// const completedTasks = [
+//   {
+//     id: 9,
+//     title: "Project Kick-off Meeting",
+//     description:
+//       "Successfully initiated the new project with all stakeholders.",
+//     date: "2025-06-29",
+//     priority: "Low",
+//   },
+//   {
+//     id: 10,
+//     title: "Initial Market Research Report",
+//     description: "Comprehensive report on initial market findings submitted.",
+//     date: "2025-06-29",
+//     priority: "Low",
+//   },
+// ];
 
 const Mainboard: FC = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [colors, setColors] = useState("");
-  const [boards, setBoards] = useState<ProjectProps[]>([
-    { projectTitle: "Todo", tasks: todoTasks, borderColors: "border-red-500" },
-    {
-      projectTitle: "In Progress",
-      tasks: inprogressTasks,
-      borderColors: "border-blue-500",
-    },
-    {
-      projectTitle: "Under Review",
-      tasks: reviewTasks,
-      borderColors: "border-pink-500",
-    },
-    {
-      projectTitle: "Completed",
-      tasks: completedTasks,
-      borderColors: "border-yellow-500",
-    },
-  ]);
+  const [boards, setBoards] = useState<ProjectProps[]>([]);
 
   const name = localStorage.getItem("userName");
 
@@ -140,35 +124,66 @@ const Mainboard: FC = () => {
     }
     setModalOpen(false);
   };
+  let content;
+  if (boards.length > 0) {
+    content = (
+      <div className="mt-8 flex flex-wrap gap-12 px-4">
+        {boards.map((board, index) => (
+          <Projectboard
+            key={index}
+            projectTitle={board.projectTitle}
+            tasks={board.tasks}
+            borderColors={board.borderColors}
+          />
+        ))}
+      </div>
+    );
+  } else {
+    content = (
+      <div className="flex justify-center">
+        <div className="bg-[#121212] flex flex-col justify-center items-center mt-12 px-12 py-8 h-[500px] w-[480px]">
+          <img src={boardImg} alt="" className="w-[200px] h-[200px]" />
+          <h1 className="text-white font-bold text-4xl mb-8 mt-4 text-center">
+            No Boards Yet!
+          </h1>
+          <p className="text-gray-200 text-xl text-center">
+            It looks like you haven't created any Kanban boards. Start
+            organizing your tasks and projects by adding your first board.
+          </p>
+          <button
+            className="bg-[#af74d7] text-white rounded-xl w-[200px] h-[40px] py-2 px-4 cursor-pointer hover:bg-[#c885f5] mt-4 mb-2"
+            onClick={() => setModalOpen(true)}
+          >
+            Add Your First Board
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
     <>
-      <div className="h-full bg-black px-8 py-12">
+      <div className="h-screen bg-black px-8 py-12">
         <h1 className="text-white text-3xl mb-8">
           Welcome Back,{" "}
           <span className="text-shadow-purple uppercase text-[#af74d7]">
             {name}
           </span>
         </h1>
-        <div className="bg-[#121212] w-[100%] px-4 py-6 h-17 flex justify-between rounded mt-2 items-center">
-          <h1 className="text-white font-bold text-2xl">My Boards</h1>
-          <button
-            className="w-50 h-10 py-2 px-4 rounded text-white bg-[#af74d7] cursor-pointer hover:bg-[#944fc5] flex"
-            onClick={openModalHandler}
-          >
-            <Plus /> Add New Board
-          </button>
-        </div>
+        {boards.length > 0 && (
+          <>
+            <div className="bg-[#121212] w-[100%] px-4 py-6 h-17 flex justify-between rounded mt-2 items-center">
+              <h1 className="text-white font-bold text-2xl">My Boards</h1>
+              <button
+                className="w-50 h-10 py-2 px-4 rounded text-white bg-[#af74d7] cursor-pointer hover:bg-[#944fc5] flex"
+                onClick={openModalHandler}
+              >
+                <Plus /> Add New Board
+              </button>
+            </div>
+          </>
+        )}
 
-        <div className="mt-8 flex flex-wrap gap-12 px-4">
-          {boards.map((board, index) => (
-            <Projectboard
-              key={index}
-              projectTitle={board.projectTitle}
-              tasks={board.tasks}
-              borderColors={board.borderColors}
-            />
-          ))}
-        </div>
+        {content}
 
         <Modal
           title="Create New Board"
