ee7aad313469a11d6f9a7874dfdb0fbf3a921d42
Deztini <agbatordestiny26@gmail.com>
2025-08-11 14:49:19 +0100

    fix: Adjusted the styling of the sidebar

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -3,5 +3,8 @@ import { ThemeContext } from "../store/context/Theme-Context";
 
 export const useTheme = () => {
   const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeContext.Provider");
+  }
   return context;
 };
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,17 +2,26 @@ import type { FC } from "react";
 import Sidebar from "../components/Layout/Sidebar";
 import logo from "../assets/projectpulselogo.png";
 import { Outlet, useNavigate } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
 
 const RootLayout: FC = () => {
   const navigate = useNavigate();
+  const { theme } = useTheme();
   return (
     <div className="flex  min-h-screen">
-      <div className=" w-[240px] flex-1/6">
+      <div
+        className={`${
+          theme === "dark" ? "bg-[#121212]" : ""
+        } w-[240px] flex-1/6`}
+      >
         <Sidebar />
       </div>
 
       <main className="flex-1 flex flex-col px-6 py-4">
-        <header className="w-full mb-0 cursor-pointer" onClick={() => navigate("/")}>
+        <header
+          className="w-full mb-0 cursor-pointer"
+          onClick={() => navigate("/")}
+        >
           <img className="w-25 h-auto" src={logo} alt="" />
         </header>
 
