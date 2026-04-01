543f45bb1dd1fe4164b384e937142a9057e30a65
Agbator Destiny <149808989+Deztini@users.noreply.github.com>
2026-04-01 23:56:12 +0100

    chore: add FlakeyRadar file (scripts/flakeyradar-runner.js)

---
+++ scripts/flakeyradar-runner.js
@@ -0,0 +1,32 @@
+const { detectFramework } = require("./core/detectFramework");
+const { parseTestResults } = require("./core/parseTestResults");
+const { rerunTests } = require("./core/rerunTests");
+const { runTests } = require("./core/runTests");
+const { writeJSON } = require("./utils/file");
+
+async function main() {
+  let fileMap = {
+    jest: "jest-results.json",
+    mocha: "mocha-results.json",
+    vitest: "vitest-results.json",
+    playwright: "playwright-results.json",
+    cypress: "mochawesome-report/mochawesome.json",
+    default: "default-results.json"
+  };
+
+  console.log("FlakeyRadar started...");
+
+  const framework = detectFramework();
+  console.log("Detected Framework: ", framework);
+
+  const rawOutput = runTests(framework);
+
+  const filePath = fileMap[framework];
+  const failedTest = parseTestResults(filePath, framework);
+  const flakyResults = await rerunTests(failedTest, framework);
+  writeJSON("flaky-results.json", flakyResults);
+
+  console.log("Flaky results saved");
+}
+
+main();
